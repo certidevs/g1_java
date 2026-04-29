f50e43132c130528fe287ba1f0b0b0065997c654
JoseMedelG <josemedel2748@gmail.com>
2026-04-29 11:40:31 +0200

    #4 TicketController created

---
+++ src/main/java/com/demo/controller/TicketController.java
@@ -0,0 +1,26 @@
+package com.demo.controller;
+
+import com.demo.repository.TicketRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@AllArgsConstructor
+@Controller
+public class TicketController {
+     // Inyectar TicketRepository
+    private final TicketRepository ticketRepository;
+
+    @GetMapping("tickets")
+    public String tickets(Model model) {
+        return "";
+    }
+
+
+
+
+
+
+}
