70f1edad6b08863b262709410fd3212436ca868a
Pablo Jara <41595956+jaranine@users.noreply.github.com>
2026-04-30 12:30:05 +0200

    #1 añadir filtros al repositorio de Movie

--- src/main/java/com/demo/repository/MovieRepository.java
+++ src/main/java/com/demo/repository/MovieRepository.java
@@ -1,7 +1,23 @@
 package com.demo.repository;
 
 import com.demo.model.Movie;
+import com.demo.model.enums.Director;
+import com.demo.model.enums.Genre;
+import com.demo.model.enums.MinAge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findBySession_StartTime(LocalDateTime startTime);
+
+    List<Movie> findByGenres(Set<Genre> genres);
+
+    List<Movie> findByDirector(Director director);
+
+    List<Movie> findByMinAge(MinAge minAge);
+
+
 }
