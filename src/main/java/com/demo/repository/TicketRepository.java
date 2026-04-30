6c36c68e8f6fbfd2afde4454085e3f00ff22c322
JoseMedelG <josemedel2748@gmail.com>
2026-04-30 09:49:40 +0200

    #4 Filters bySession_movie_Id and purchaseTime created

--- src/main/java/com/demo/repository/TicketRepository.java
+++ src/main/java/com/demo/repository/TicketRepository.java
@@ -3,5 +3,19 @@ package com.demo.repository;
 import com.demo.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    // Para buscar tickets vendidos por pelicula
+    List<Ticket> findBySession_Movie_Id(Long id);
+
+    // Para buscar tickets vendidos por fecha y hora
+    List<Ticket> findByPurchaseTime(LocalDateTime purchaseTime);
+
+
+
+
+
 }
