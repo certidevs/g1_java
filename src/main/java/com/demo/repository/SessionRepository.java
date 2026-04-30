ba060ac4906ecbdce44618bd4acc086e8ca40753
Barbara Urbano <sbaarby@gmail.com>
2026-04-30 10:18:26 +0200

    #3 Se agregan los filtros en Session repository

--- src/main/java/com/demo/repository/SessionRepository.java
+++ src/main/java/com/demo/repository/SessionRepository.java
@@ -1,7 +1,15 @@
 package com.demo.repository;
 
 import com.demo.model.Session;
+import com.demo.model.enums.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SessionRepository extends JpaRepository<Session, Long> {
+    List<Session> findByMovie_Id(Long id);
+
+    List<Session> findByLanguage(Language language);
+
+
 }
