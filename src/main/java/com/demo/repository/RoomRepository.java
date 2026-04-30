a9ad0faff5ebdd0a4343cf777cabe1bd418753ae
hakimh4 <hakimherba999@gmail.com>
2026-04-30 13:30:33 +0200

    #2 añadido list RoomRepository

--- src/main/java/com/demo/repository/RoomRepository.java
+++ src/main/java/com/demo/repository/RoomRepository.java
@@ -1,7 +1,18 @@
 package com.demo.repository;
 
 import com.demo.model.Room;
+import com.demo.model.enums.ScreenType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+    // List<Room> findByNameContains(String name);
+
+    // filtrar por número de butacas
+    List<Room> findByCapacityGreaterThanEqual(Integer capacity);
+
+    // filtrar por tipo de pantalla
+    List<Room> findByScreentype(ScreenType screentype);
 }
