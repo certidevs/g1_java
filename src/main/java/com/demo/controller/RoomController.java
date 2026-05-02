322bee2e4e335313cff7221b6c5c0f129daf511b
hakimh4 <hakimherba999@gmail.com>
2026-05-02 17:07:24 +0200

    #2 agregado los filtros del repositorio en el RoomController

--- src/main/java/com/demo/controller/RoomController.java
+++ src/main/java/com/demo/controller/RoomController.java
@@ -1,21 +1,43 @@
 package com.demo.controller;
 
+import com.demo.model.enums.ScreenType;
 import com.demo.repository.RoomRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @AllArgsConstructor
 public class RoomController {
+        private final RoomRepository roomRepository;
 
-    // inyectar el room repository
-    private final RoomRepository roomRepository;
-
-    //getmapping rooms
+    // Lista todas las salas
     @GetMapping("/rooms")
-    public String roomsList(Model model){
+    public String roomsList(Model model) {
         model.addAttribute("rooms", roomRepository.findAll());
-        return "rooms/room-list";}
+        return "rooms/room-list";
+    }
+
+    // Detalle de una sala por ID
+    @GetMapping("/rooms/{id}")
+    public String roomDetail(Model model, @PathVariable Long id) {
+        model.addAttribute("room", roomRepository.findById(id).orElse(null));
+        return "rooms/room-detail";
+    }
+
+    // Filtrar por capacidad mayor o igual que
+    @GetMapping("/rooms/{capacity}")
+    public String roomsListCapacity(Model model, @PathVariable Integer capacity) {
+        model.addAttribute("rooms", roomRepository.findByCapacityGreaterThanEqual(capacity));
+        return "rooms/room-list";
+    }
+
+    // Filtrar por tipo de pantalla
+    @GetMapping("/rooms/{screenType}")
+    public String roomsListScreenType(Model model, @PathVariable ScreenType screenType) {
+        model.addAttribute("rooms", roomRepository.findByScreentype(screenType));
+        return "rooms/room-list";
+    }
 }
--- src/main/java/com/demo/repository/RoomRepository.java
+++ src/main/java/com/demo/repository/RoomRepository.java
@@ -5,10 +5,7 @@ import com.demo.model.enums.ScreenType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
 public interface RoomRepository extends JpaRepository<Room, Long> {
-
-    // List<Room> findByNameContains(String name);
 
     // filtrar por número de butacas
     List<Room> findByCapacityGreaterThanEqual(Integer capacity);
