87eb8eecb6765cd02d6a7faa1f04d5bc10feb29d
Barbara Urbano <sbaarby@gmail.com>
2026-05-04 00:38:42 +0200

    #3 Se muestan en el html todas las sessiones de la pelicula 1

--- src/main/java/com/demo/controller/SessionController.java
+++ src/main/java/com/demo/controller/SessionController.java
@@ -1,5 +1,6 @@
 package com.demo.controller;
 
+import com.demo.model.Session;
 import com.demo.model.enums.Language;
 import com.demo.repository.SessionRepository;
 import lombok.AllArgsConstructor;
@@ -8,28 +9,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 @AllArgsConstructor
 public class SessionController {
-    //Aqui agregar el getmapping y demas
+    //Aqui inyectar los repository que se necesitaran
     private final SessionRepository sessionRepository;
+    private final MovieController movieController;
+    private final RoomController roomController;
 
-    //Estoy intentando traer todas las sesiones
+    //[OK]Todas las sesiones
+    //Este es el CONTROLADOR
     @GetMapping("sessions")
     public String sessions(Model model){
-        model.addAttribute("sessions", sessionRepository.findAll());
+        //Cargamos los datos en el MODELO
+        List<Session> cinemaFuntions = sessionRepository.findAll();
+        model.addAttribute("cinemaFunctions", cinemaFuntions);
+        //Retornamos a la VISTA
         return "sessions/session-list";
     }
-    //Estoy intentando traer las sessiones que tiene esa pelicula.
-    @GetMapping("sessions/{id}")
-    public String sessionDetail(@PathVariable Long id, Model model){
-        model.addAttribute("session", sessionRepository.findByMovie_Id(id));
-        return "sessions/session-detail";
-    }
-    //Estoy intentando traer las sesiones de las peliculas que tienen UN lenguaje especifico, por ejemplo VOSE
-    @GetMapping("sessions/{Languege}")
-    public String sessionsByLanguage(Model model, @PathVariable Language Languege){
-        model.addAttribute("sessionsLanguaje", sessionRepository.findByLanguage(Languege));
-        return "sessions/session-list";
-    }
+
+    //El detalle de la session
+    //Las sesiones que tienen UN lenguaje especifico, por ejemplo VOSE
+    //Las sesiones que tengan UN tipo de pantalla por ejemplo 3D
+
+
+
 }
