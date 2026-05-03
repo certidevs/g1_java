07de4362bb6facb795f7a2abf1f5f384e0fe67bc
hakimh4 <hakimherba999@gmail.com>
2026-05-03 13:11:23 +0200

    #2 agregado htmls de Room y sacar datos en RoomController

--- src/main/java/com/demo/controller/RoomController.java
+++ src/main/java/com/demo/controller/RoomController.java
@@ -1,5 +1,6 @@
 package com.demo.controller;
 
+import com.demo.model.Room;
 import com.demo.model.enums.ScreenType;
 import com.demo.repository.RoomRepository;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -16,7 +19,10 @@ public class RoomController {
     // Lista todas las salas
     @GetMapping("/rooms")
     public String roomsList(Model model) {
+        List<Room> rooms = roomRepository.findAll();
         model.addAttribute("rooms", roomRepository.findAll());
+        model.addAttribute("numRooms", rooms.size());
+        model.addAttribute("title", "Lista de salas");
         return "rooms/room-list";
     }
 
@@ -28,16 +34,22 @@ public class RoomController {
     }
 
     // Filtrar por capacidad mayor o igual que
-    @GetMapping("/rooms/{capacity}")
+    @GetMapping("/rooms/capacity/{capacity}")
     public String roomsListCapacity(Model model, @PathVariable Integer capacity) {
         model.addAttribute("rooms", roomRepository.findByCapacityGreaterThanEqual(capacity));
+        model.addAttribute("numRooms", roomRepository.findByCapacityGreaterThanEqual(capacity).size());
+        model.addAttribute("title", "Salas con capacidad mayor o igual a " + capacity);
         return "rooms/room-list";
     }
 
     // Filtrar por tipo de pantalla
-    @GetMapping("/rooms/{screenType}")
+    @GetMapping("/rooms/screentype/{screenType}")
     public String roomsListScreenType(Model model, @PathVariable ScreenType screenType) {
+        List<Room> rooms = roomRepository.findByScreentype(screenType);
         model.addAttribute("rooms", roomRepository.findByScreentype(screenType));
+        model.addAttribute("numRooms", rooms.size());
+        model.addAttribute("title", "Salas con tipo de pantalla " + screenType);
         return "rooms/room-list";
+
     }
 }
