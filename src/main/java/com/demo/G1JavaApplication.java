3c502b01644c01dac0e365ce30b8ba52b7d0ddcb
Pablo Jara <41595956+jaranine@users.noreply.github.com>
2026-05-04 11:42:30 +0200

    #1 añadir mejora de maquetado y legibilidad

--- src/main/java/com/demo/G1JavaApplication.java
+++ src/main/java/com/demo/G1JavaApplication.java
@@ -36,8 +36,12 @@ public class G1JavaApplication {
         movie1.setMinAge(MinAge.AGE_7);
         movie1.setDuration(98);
         movie1.setTrailerUrl("https://youtu.be/ipzEY7c7it8");
-        movie1.setImageUrl("https://m.media-amazon.com/images/M/MV5BMDg5MjRkNWEtYmU1Mi00MTExLTk5MDQtY2RiMWVkZWNiOThjXkEyXkFqcGc@._V1_.jpg");
-        movie1.setSinopsis("Mario y Luigi ayudan a la Princesa Peach en el Reino Champiñón y conocen a un nuevo aliado: Yoshi. Cuando una celebración desencadena una aventura galáctica, los hermanos viajan al espacio para detener a Bowser Jr. y proteger a Rosalina.");
+        movie1.setImageUrl(
+                "https://m.media-amazon.com/images/M/MV5BNTRkODc0MmItYzQ2NS00MGU3LTkwMTUtMGRmZjMwODNiNmI0XkEyXkFqcGc@._V1_.jpg"
+        );
+        movie1.setSinopsis("Mario y Luigi ayudan a la Princesa Peach en el Reino Champiñón y " +
+                "conocen a un nuevo aliado: Yoshi. Cuando una celebración desencadena una aventura galáctica, " +
+                "los hermanos viajan al espacio para detener a Bowser Jr. y proteger a Rosalina.");
 
         Movie movie2 = new Movie();
         movie2.setTitle("El diablo viste de Prada 2");
@@ -47,7 +51,9 @@ public class G1JavaApplication {
         movie2.setMinAge(MinAge.AGE_7);
         movie2.setDuration(119);
         movie2.setTrailerUrl("https://youtu.be/IQD3qh7te2o");
-        movie2.setImageUrl("https://m.media-amazon.com/images/M/MV5BOGEyNGU5YzItY2VhMy00NTRmLWI5MmYtZDYxZTczMjcwN2E3XkEyXkFqcGc@._V1_.jpg");
+        movie2.setImageUrl(
+                "https://m.media-amazon.com/images/M/MV5BOGEyNGU5YzItY2VhMy00NTRmLWI5MmYtZDYxZTczMjcwN2E3XkEyXkFqcGc@._V1_.jpg"
+        );
         movie2.setSinopsis("Cuando Miranda Priestly está por retirarse, " +
                 "se reencuentra con Andy Sachs para enfrentarse a su exasistente convertida en rival: Emily Charlton.");
 
@@ -59,7 +65,9 @@ public class G1JavaApplication {
         movie3.setMinAge(MinAge.AGE_12);
         movie3.setDuration(127);
         movie3.setTrailerUrl("https://youtu.be/iVJaujA54Y8");
-        movie3.setImageUrl("https://m.media-amazon.com/images/M/MV5BYmMzNTNkOGYtYWViYy00ZmI2LWJjZDQtODhhNTA4NmRjNmE0XkEyXkFqcGc@._V1_.jpg");
+        movie3.setImageUrl(
+                "https://m.media-amazon.com/images/M/MV5BYmMzNTNkOGYtYWViYy00ZmI2LWJjZDQtODhhNTA4NmRjNmE0XkEyXkFqcGc@._V1_.jpg"
+        );
         movie3.setSinopsis("Película biográfica sobre el rey del pop, Michael Jackson. " +
                 "Retratará al cantante desde sus inicios hasta su trágico fallecimiento en 2009.");
 
@@ -71,7 +79,9 @@ public class G1JavaApplication {
         movie4.setMinAge(MinAge.ALL);
         movie4.setDuration(87);
         movie4.setTrailerUrl("https://youtu.be/O9kD-WhRNus");
-        movie4.setImageUrl("https://m.media-amazon.com/images/M/MV5BYTFlNmEyOTItYjI4NS00MDA1LWFkNjItMWU4NTM5ODFjOTNjXkEyXkFqcGc@._V1_.jpg");
+        movie4.setImageUrl(
+                "https://m.media-amazon.com/images/M/MV5BYTFlNmEyOTItYjI4NS00MDA1LWFkNjItMWU4NTM5ODFjOTNjXkEyXkFqcGc@._V1_.jpg"
+        );
         movie4.setSinopsis("Cuando Toni Benetón cree que tiene todo bajo control en su entrañable y divertida " +
                 "familia multicultural, la llegada de dos nuevos bebés pondrá patas arriba la casa y a toda la familia. " +
                 "Los problemas crecen pero las aventuras también.");
