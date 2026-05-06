57e8c8e4a66aab0e19b865460e33d426354a02d7
hakimh4 <hakimherba999@gmail.com>
2026-05-06 20:11:36 +0200

    #2 Añadido label a enum ScreenType

--- src/main/java/com/demo/model/enums/ScreenType.java
+++ src/main/java/com/demo/model/enums/ScreenType.java
@@ -1,8 +1,18 @@
 package com.demo.model.enums;
 
 public enum ScreenType {
-    STANDARD,
-    IMAX,
-    THREE_D,
-    VIP
+    STANDARD("Estándar"),
+    IMAX("2D"),
+    THREE_D("3D"),
+    VIP("VIP");
+
+    private final String label;
+
+    ScreenType(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
