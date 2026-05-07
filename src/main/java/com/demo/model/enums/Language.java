bd14a2d551f39b0e477dec22f2815c0f697d138e
Barbara Urbano <sbaarby@gmail.com>
2026-05-07 14:07:12 +0200

    #3 Se agrega el label en el enum de Language

--- src/main/java/com/demo/model/enums/Language.java
+++ src/main/java/com/demo/model/enums/Language.java
@@ -1,15 +1,23 @@
 package com.demo.model.enums;
 
 public enum Language {
-    VO, VOS, VOSE, VOSI, DOB, SUB, CAST, LAT, ESP
-}
+    VO("Version original (sin sibtitulo)"),
+    VOS("Version original subtitulada"),
+    VOSE("Version original subtitulada en español"),
+    VOSI("Version original subtitulada en ingles"),
+    DOB("Doblada"),
+    SUB("Subtitulada"),
+    CAST("Castellano (Español de España)"),
+    LAT("Latino"),
+    ESP("Español (Generico)");
 
-//VO --> Version original (sin sibtitulo)
-//VOS --> Version original subtitulada
-//VOSE --> Version original subtitulada en español
-//VOSI --> Version original subtitulada en ingles
-//DOB --> Doblada
-//SUB --> Subtitulada
-//CAST --> Castellano (Español de España)
-//LAT ---> Latino
-//ESP ---> Español (Generico)
+    private final String label;
+
+    Language(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
