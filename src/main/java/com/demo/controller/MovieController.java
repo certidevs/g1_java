453df05f53788c1825217cbe9cd0e73b7b2da286
Pablo Jara <41595956+jaranine@users.noreply.github.com>
2026-05-08 19:56:38 +0200

    #1 añadir formulario y edición de películas

--- src/main/java/com/demo/controller/MovieController.java
+++ src/main/java/com/demo/controller/MovieController.java
@@ -1,12 +1,17 @@
 package com.demo.controller;
 
 import com.demo.model.Movie;
+import com.demo.model.enums.Director;
+import com.demo.model.enums.Genre;
+import com.demo.model.enums.MinAge;
 import com.demo.repository.MovieRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,5 +35,30 @@ public class MovieController {
     public String movie(Model model, @PathVariable Long id){
         model.addAttribute("movie", movieRepository.findById(id).orElseThrow());
         return "movies/movie-detail";
+    }
+
+    @GetMapping("movies/new")
+    public String newMovie(Model model) {
+        model.addAttribute("movie", new Movie());
+        model.addAttribute("genres", Genre.values());
+        model.addAttribute("directors", Director.values());
+        model.addAttribute("minAges", MinAge.values());
+        return "movies/movie-form";
+    }
+
+    @GetMapping("movies/edit/{id}")
+    public String editMovie(@PathVariable Long id, Model model) {
+        model.addAttribute("movie", movieRepository.findById(id).orElseThrow());
+        model.addAttribute("genres", Genre.values());
+        model.addAttribute("directors", Director.values());
+        model.addAttribute("minAges", MinAge.values());
+        return "movies/movie-form";
+    }
+
+    @PostMapping("movies")
+    public String saveMovie(@ModelAttribute Movie movie) {
+        System.out.println("Película guardada: " + movie);
+        movieRepository.save(movie);
+        return "redirect:/movies/" + movie.getId();
     }
 }
