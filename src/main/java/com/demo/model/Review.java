fa3eb8ae851fe63ba18109b1ee4ad7c96f10b063
JoseMedelG <josemedel2748@gmail.com>
2026-05-09 18:18:17 +0200

    #5 Review.java, ReviewRepository, review-list.html y review-detail.html creadas con filtro

---
+++ src/main/java/com/demo/model/Review.java
@@ -0,0 +1,37 @@
+package com.demo.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    private Integer rating;
+
+    @Builder.Default // para que el builder no ponga este campo a null
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    @ToString.Exclude
+    @ManyToOne
+    private Movie movie;
+
+}
