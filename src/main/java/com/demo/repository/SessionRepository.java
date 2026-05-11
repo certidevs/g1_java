60cded83633d3b75642599b309be923c610ba810
Barbara Urbano <sbaarby@gmail.com>
2026-05-11 02:25:28 +0200

    #3 Se agrega la query para utilizar los filtros en el controller

--- src/main/java/com/demo/repository/SessionRepository.java
+++ src/main/java/com/demo/repository/SessionRepository.java
@@ -4,6 +4,8 @@ import com.demo.model.Session;
 import com.demo.model.enums.Language;
 import com.demo.model.enums.ScreenType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,5 +19,17 @@ public interface SessionRepository extends JpaRepository<Session, Long> {
 
     List<Session> findByRoom_Screentype(ScreenType screentype);
 
+    @Query("""
+      SELECT s FROM Session s
+      WHERE s.active = true
+      AND (:language IS NULL OR s.language = :language)
+      AND (:screentype IS NULL OR s.room.screentype = :screentype)
+      AND (:title IS NULL OR :title = '' OR LOWER(s.movie.title) LIKE LOWER(CONCAT('%', :title, '%')))
+      """)
+    List<Session> findActiveFiltering(
+            @Param("language") Language language,
+            @Param("title") String title,
+            @Param("screentype") ScreenType screentype
+    );
 
 }
