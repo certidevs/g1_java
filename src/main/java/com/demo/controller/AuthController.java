5bdaf64e116af2bd42a4bbe1ed738d2203b5e4f6
hakimh4 <hakimherba999@gmail.com>
2026-05-19 20:14:41 +0200

    #9 crear package dto y agregar AuthController con RegisterForm DTO

--- src/main/java/com/demo/controller/AuthController.java
+++ src/main/java/com/demo/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.demo.controller;
 
+import com.demo.dto.RegisterForm;
 import com.demo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
@@ -12,9 +17,30 @@ public class AuthController {
     private final UserService userService;
 
     // @GetMapping /register
+    // Navegar a formulario de registro
+    @GetMapping("register")
+    public String register(Model model) {
+        // opcion 1: entidad User
+        // model.addAttribute("user", new User());
+        // opcion 2: dto UserRegisterDTO
+        model.addAttribute("user", new RegisterForm());
+        return "auth/register";
+    }
 
     // @PostMapping /register
+    @PostMapping("register")
+    public String register(@ModelAttribute RegisterForm form){
+        System.out.println(form);
+        // verificar si username ocupado
+        // verificar si email ocupado
+        // verificar password
+        return "redirect:/login";
+    }
 
     // @GetMapping  /login
+    @GetMapping("login")
+    public String login() {
+        return "auth/login";
+    }
 
 }
