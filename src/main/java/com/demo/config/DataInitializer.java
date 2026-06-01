2e92bb8364be8f99bfff62973360e0a49f2f71f4
JoseMedelG <josemedel2748@gmail.com>
2026-06-01 18:52:32 +0200

    #15 creada movie demo en DataInitializer para favs en user-detail

--- src/main/java/com/demo/config/DataInitializer.java
+++ src/main/java/com/demo/config/DataInitializer.java
@@ -1,15 +1,23 @@
 package com.demo.config;
 
 import com.demo.dto.RegisterForm;
+import com.demo.model.Favorite;
+import com.demo.model.Movie;
 import com.demo.model.User;
+import com.demo.model.enums.Genre;
 import com.demo.model.enums.Role;
+import com.demo.repository.FavoriteRepository;
+import com.demo.repository.MovieRepository;
 import com.demo.repository.UserRepository;
+import com.demo.service.FavoriteService;
 import com.demo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.EnumSet;
 
 /*
 Clase para insertar datos demo en el entorno de desarrollo
@@ -18,6 +26,8 @@ Clase para insertar datos demo en el entorno de desarrollo
 @AllArgsConstructor
 public class DataInitializer implements ApplicationRunner {
 
+    private final FavoriteRepository favoriteRepository;
+    private final MovieRepository movieRepository;
     private final UserService userService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -49,6 +59,22 @@ public class DataInitializer implements ApplicationRunner {
         // startTime LocalDateTime.now().plusDays(1)
         // startTime LocalDateTime.now().plusDays(2)
         // startTime LocalDateTime.now().plusDays(3)
+
+
+        // Crear una movie
+        Movie movie = new Movie();
+        movie.setTitle("Película para fav");
+        movie.setActive(Boolean.TRUE);
+        movie.setDuration(120);
+        movie.setGenreSet(EnumSet.of(Genre.ADVENTURE, Genre.COMEDY));
+        movie.setImageUrl("/uploads/ollama.png");
+        movieRepository.save(movie);
+
+        favoriteRepository.save(Favorite.builder().movie(movie).user(user).build());
+
+        // guardarlos como favoritos de un usuario
+        // así los podemos mostrar en el user-detail.html
+
 
     }
 }
